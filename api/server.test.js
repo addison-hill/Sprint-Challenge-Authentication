72be246d3052efb08fee5133f1c17e0902e3916c
Addison Hill <adddison.hill@gmail.com>
2019-12-20 11:41:44 -0600

    tests for server, login, register all passing with sanity checks

--- api/server.test.js
+++ api/server.test.js
@@ -19,4 +19,35 @@ describe("server", function() {
         expect(res.status).toBe(201);
       });
   });
+
+  it("should return a username", function() {
+    return request(server)
+      .post("/api/auth/register")
+      .send({ username: "Emily", password: "Vivacode" })
+      .then(res => {
+        expect(res.body.username).toEqual("Emily");
+      });
+  });
+
+  it("should return 200 on login", async () => {
+    // User registers
+    const response = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "Addy", password: "pass" });
+
+    // User logs in
+    const loginResponse = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Addy", password: "pass" });
+    expect(loginResponse.status).toBe(200);
+  });
+
+  it("Cant login with Invalid Credentials", async () => {
+    const response = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "NotAddy", password: "Notpass" })
+      .then(res => {
+        expect(res.status).toBe(401);
+      });
+  });
 });
