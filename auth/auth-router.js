329974565a764d93e1392d2faf0270f5a3adb58b
Addison Hill <adddison.hill@gmail.com>
2019-12-20 10:25:31 -0600

    register and login endpoints built out, users model created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,58 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("../users/users-model");
+
+router.post("/register", (req, res) => {
+  let { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
+
+// Sign Token
+function signToken(user) {
+  const payload = {
+    username: user.username,
+    id: user.id
+  };
+
+  const secret = "is it secret, is it safe?";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
