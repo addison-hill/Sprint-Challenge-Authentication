994d1fa2e97235a1974b34f6ce000259edc89f83
Addison Hill <adddison.hill@gmail.com>
2019-12-20 10:32:57 -0600

    authentication and validateUser middleware built out

--- users/users-helpers.js
+++ users/users-helpers.js
@@ -1,1 +1,13 @@
+module.exports = {
+  validateUser
+};
 
+function validateUser(req, res, next) {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    next();
+  } else {
+    res.status(400).json({ message: "Please provide username and password" });
+  }
+}
